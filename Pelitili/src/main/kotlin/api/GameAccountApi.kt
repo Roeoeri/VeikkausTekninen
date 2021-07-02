0a9a7bf1ab35f4ec5aae2ef92462bbee7020646a
Roeoeri <tomi.haapakoski@helsinki.fi>
2021-07-03 00:43:56 +0300

    Add: Api for GameAccount transactions

--- Pelitili/src/main/kotlin/api/GameAccountApi.kt
+++ Pelitili/src/main/kotlin/api/GameAccountApi.kt
@@ -1,15 +1,51 @@
 package api
+import gameaccount.AccountBalanceResponse
 import gameaccount.GameAccount
 import io.javalin.Javalin
 
-class GameAccountApi(port:Int, storage: GameAccount) {
+class GameAccountApi(val port: Int, val storage: GameAccount) {
 
-    val port = port
-    val storage = storage
+    data class DepositOrChargeRequest(val gameEventId:String, val playerId: String, val amount: Int)
+    data class ErrorResponse(val message:String)
+    data class SuccessResponse(val balance:Int)
 
     fun startApi(){
         val app  = Javalin.create().start(port)
-        app.get("/") { ctx -> ctx.json(storage.getPlayerAccounts())}
+        app.get("/players") { ctx ->
+            println("Asd")
+            ctx.json(storage.getPlayerAccounts())}
+        app.get("/gameEvents") { ctx -> ctx.json(storage.getGameEvents())}
+
+        app.post("/api/deposit"){ ctx ->
+            val params = ctx.body<DepositOrChargeRequest>()
+            when(val transactionEvent = storage.depositPlayerAccount(params.gameEventId, params.playerId, params.amount)){
+                is AccountBalanceResponse.Error -> {
+                    ctx.status(400)
+                    ctx.json(ErrorResponse(transactionEvent.errorMessage))
+
+                }
+                is AccountBalanceResponse.Success -> {
+                    ctx.status(200)
+                    ctx.json(SuccessResponse(transactionEvent.balance))
+                }
+            }
+        }
+
+        app.post("/api/charge"){ ctx ->
+            val params = ctx.body<DepositOrChargeRequest>()
+            when(val transactionEvent = storage.chargePlayerAccount(params.gameEventId, params.playerId, params.amount)){
+                is AccountBalanceResponse.Error -> {
+                    ctx.status(400)
+                    ctx.json(ErrorResponse(transactionEvent.errorMessage))
+
+                }
+                is AccountBalanceResponse.Success -> {
+                    ctx.status(200)
+                    ctx.json(SuccessResponse(transactionEvent.balance))
+                }
+            }
+        }
+
     }
 
 }
