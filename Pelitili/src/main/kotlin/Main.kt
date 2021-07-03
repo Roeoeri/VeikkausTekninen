4c2622809cdd3fe0e1821454bd09f7de3e37d9ce
Roeoeri <tomi.haapakoski@helsinki.fi>
2021-07-03 16:55:10 +0300

    Add: port for heroku

--- Pelitili/src/main/kotlin/Main.kt
+++ Pelitili/src/main/kotlin/Main.kt
@@ -7,10 +7,19 @@ fun main() {
 
     val stamper = LocalStamper()
     val storage = GameAccountDSL(stamper)
+
+    val parsePort = System.getenv("PORT").toIntOrNull()
+    var port: Int = 3000
+    
+    when(parsePort){
+        is Int -> {port = parsePort}
+        
+    }
+
     storage.createPlayerAccount("111qqq", "Pekka", 10000)
     storage.createPlayerAccount("222qqq", "Harri", 20000)
     storage.createPlayerAccount("333qqq", "Jussi", 30000)
-    val api = GameAccountApi(3000, storage)
+    val api = GameAccountApi(port, storage)
     api.startApi()
 
 }
